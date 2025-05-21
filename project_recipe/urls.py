2b93a6e878a88f1b1530514fdde7dda52af06415
anntulyholovets <anntulyholovets@knu.com>
2025-05-21 17:28:53 +0300

    Додано маршрути для main та category_detail

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from recipe import views  # імпортуємо views
+from recipe import views  # Імпортуємо наші view-функції
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.main, name='main'),  # головна сторінка — view "main"
+    path('', views.main, name='main'),  # головна сторінка
+    path('category/<int:category_id>/', views.category_detail, name='category_detail'),  # сторінка категорії
 ]
