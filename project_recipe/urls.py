4e3cc5b2528b382833519267625b5ec088c1cc93
anntulyholovets <anntulyholovets@knu.com>
2025-05-21 17:23:38 +0300

    Додано view main для відображення 10 випадкових рецептів

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-
+from recipe import views  # імпортуємо views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.main, name='main'),  # головна сторінка — view "main"
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,8 @@
+from django.shortcuts import render
+from .models import Recipe
+import random
 
+def main(request):
+    all_recipes = list(Recipe.objects.all())
+    random_recipes = random.sample(all_recipes, min(len(all_recipes), 10))  # до 10
+    return render(request, 'main.html', {'recipes': random_recipes})
